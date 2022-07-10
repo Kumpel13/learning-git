2b7ac48780adeb918a65f6e4d0d88465095eb60e
Adrian Telus <adrian.telus13@gmail.com>
2022-07-10 19:14:18 +0200

    Add GitTutorial.py

---
+++ GitTutorial.py
@@ -0,0 +1,8 @@
+shopping_list = {
+    "piekarnia": ["chleb", "pączek", "bułki"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+}
+
+for key, value in shopping_list.items():
+    print(f"Idę do {key}, kupuję tu następujące rzeczy: {value}")
+
