321a563da78fa41f5c6705caea9b24064288c426
Adrian Telus <adrian.telus13@gmail.com>
2022-07-10 19:47:49 +0200

    Add Len function to sum shopping

--- InitProject.py
+++ InitProject.py
@@ -8,4 +8,6 @@ for key, value in shopping_list.items():
     key = key.capitalize()
     print(f"Idę do {key}, kupuję tu następujące rzeczy: {value}")
 
+x = len(shopping_list["piekarnia"]) + len(shopping_list["warzywniak"])
+
 print(f"W sumie kupuję {x} produktów")
