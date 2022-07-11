8c0f15e1b4643b1fb0b9df19e0edd68963abf18e
Kumpel13 <109043133+Kumpel13@users.noreply.github.com>
2022-07-11 08:42:57 +0200

    Add simple loop

--- InitProject.py
+++ InitProject.py
@@ -11,3 +11,7 @@ for key, value in shopping_list.items():
 x = len(shopping_list["piekarnia"]) + len(shopping_list["warzywniak"])
 
 print(f"W sumie kupuję {x} produktów")
+
+for i in range (1,10):
+    if i % 2 ==0:
+        print(i)
